06567dcf467c9d6014eede514e12a69d276867c1
tonandton <www.sorasakok@gmail.com>
2022-04-17 12:18:31 +0700

    Add Stars and Forks

--- src/context/context.js
+++ src/context/context.js
@@ -9,15 +9,37 @@ const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
 
 // Provider, Consumer - GithubContext.Provider
-
 const GithubProvider = ({ children }) => {
     // console.log(children);
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
 
+    // request loading
+    const [requests, setRequests] = useState(0);
+    const [loading, setIsLoading] = useState(false);
+    // check rate
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`)
+            .then(({ data }) => {
+                let {
+                    rate: { remaining },
+                } = data;
+                setRequests(remaining);
+                if (remaining === 0) {
+                    // throw an error
+                }
+            })
+            .catch((err) => console.log(err));
+    };
+
+    // error
+    useEffect(checkRequests, []);
+
     return (
-        <GithubContext.Provider value={{ githubUser, repos, followers }}>
+        <GithubContext.Provider
+            value={{ githubUser, repos, followers, requests }}
+        >
             {children}
         </GithubContext.Provider>
     );
